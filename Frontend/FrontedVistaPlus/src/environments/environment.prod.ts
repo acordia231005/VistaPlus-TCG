a9536542124d832fa152dadb753b3eb0055661a5
23gordo05 <andrescortesdiaz2005@gmail.com>
2026-05-17 14:11:23 +0200

    env cambiados

--- Frontend/FrontedVistaPlus/src/environments/environment.prod.ts
+++ Frontend/FrontedVistaPlus/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: (typeof window !== 'undefined' ? (window as any)['env']?.apiUrl : null)
-    || 'http://192.168.0.112:8080'
+    || 'http://192.168.0.112:8080/VistaPlus'
 };
