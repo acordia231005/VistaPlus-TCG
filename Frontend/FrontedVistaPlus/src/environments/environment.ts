d2c67f17b7ed461e67f820cd9a2442611d1fa69e
Andrés José Cortés Díaz <andrescortesdiaz2005@gmail.com>
2026-05-18 10:31:04 +0200

    cambios finales

--- Frontend/FrontedVistaPlus/src/environments/environment.ts
+++ Frontend/FrontedVistaPlus/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: (typeof window !== 'undefined' ? (window as any)['env']?.apiUrl : null)
+  apiUrl: (typeof window !== 'undefined' ? ((window as any)['env']?.apiUrl || (window as any)['env']?.API_URL) : null)
     || 'http://192.168.9.150:8080'
 };
